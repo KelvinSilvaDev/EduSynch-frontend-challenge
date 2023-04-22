bfa24b7baf928336951a8057b3f302840e59090d
Kelvin Oliveira <63024154+KelvinSilvaDev@users.noreply.github.com>
2023-04-22 16:22:28 -0300

    api conect

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333/",
+  baseURL: "https://rest.coinapi.io/v1/",
   headers: {
     "Content-Type": "application/json",
     "X-CoinAPI-Key": "46AFA969-07F3-4110-A335-0F0960CD4228",
